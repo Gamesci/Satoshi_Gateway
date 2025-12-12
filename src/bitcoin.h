7ffaf4816d4c26ac4bb64b47dc9fa586a5d45a3c
WuKong <govcnovo@gmail.com>
2025-12-12 23:57:35 +0800

    Update bitcoin.h

--- src/bitcoin.h
+++ src/bitcoin.h
@@ -4,19 +4,21 @@
 #include <stdint.h>
 
 typedef struct {
-    char job_id;
-    char prev_hash;
-    char bits;
-    uint32_t version;
-    uint32_t curtime;
+    char job_id[32];
+    char prev_hash[65];     // Hex string (Little Endian for Stratum)
+    char coinb1[1024];      // Hex string
+    char coinb2[1024];      // Hex string
+    char merkle_branch[20][65]; // Merkle path (Hex)
+    int merkle_count;
+    char version[9];        // Hex string
+    char nbits[9];          // Hex string
+    char ntime[9];          // Hex string
     uint32_t height;
-    int64_t coinbase_value;
     bool clean_jobs;
-    // 实际实现需要存储 coinbase 交易部分和 merkle branches
+    uint32_t target_bits;   // int representation of nbits
 } Template;
 
 int bitcoin_init();
 void bitcoin_update_template(bool clean_jobs);
 int bitcoin_submit_block(const char *hex_data);
-
 #endif

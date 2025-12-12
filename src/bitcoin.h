43f32c4c1db84d162009e6458fd4cb068f20f738
WuKong <govcnovo@gmail.com>
2025-12-13 00:57:39 +0800

    Update bitcoin.h

--- src/bitcoin.h
+++ src/bitcoin.h
@@ -16,9 +16,11 @@ typedef struct {
     char nbits[9];
     char ntime[9];
     uint32_t height;
+    
     uint32_t version_int;
     uint32_t nbits_int;
     uint32_t ntime_int;
+    
     char **tx_hexs;
     int tx_count;
     bool clean_jobs;
@@ -28,7 +30,7 @@ int bitcoin_init();
 void bitcoin_update_template(bool clean_jobs);
 int bitcoin_reconstruct_and_submit(const char *job_id, const char *full_extranonce, const char *ntime, uint32_t nonce, uint32_t version_mask);
 
-// 新增：线程安全地获取当前任务副本 (用于新连接矿机立即下发)
+// 线程安全地获取当前任务副本 (用于新连接矿机)
 bool bitcoin_get_current_job_copy(Template *out);
 
 #endif

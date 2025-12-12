6b8d998b0169554837c9a5b80099fdc527c14201
WuKong <govcnovo@gmail.com>
2025-12-13 04:49:40 +0800

    Update utils.h

--- src/utils.h
+++ src/utils.h
@@ -1,19 +1,15 @@
 #ifndef UTILS_H
 #define UTILS_H
+
 #include <stdint.h>
 #include <stddef.h>
 
-void log_info(const char *format, ...);
-void log_error(const char *format, ...);
-void log_debug(const char *format, ...);
-
-void hex2bin(const char *hex, uint8_t *bin, size_t bin_len);
-void bin2hex(const uint8_t *bin, size_t bin_len, char *hex);
+void log_info(const char *fmt, ...);
+void log_error(const char *fmt, ...);
+void hex2bin(const char *hex, uint8_t *bin, size_t len);
+void bin2hex(const uint8_t *bin, size_t len, char *hex);
 void reverse_bytes(uint8_t *buf, size_t len);
 void swap32_buffer(uint8_t *buf, size_t len);
 uint32_t swap_uint32(uint32_t val);
 
-int base58_decode_check(const char *str, uint8_t *payload, size_t *payload_len);
-int segwit_addr_decode(int* witver, uint8_t* witprog, size_t* witprog_len, const char* hrp, const char* addr);
-void address_to_script(const char *addr, char *script_hex);
 #endif

787dc35c26feafe25b9db99ff5db9aded6db3e1d
WuKong <govcnovo@gmail.com>
2025-12-13 03:36:56 +0800

    Update stratum.h

--- src/stratum.h
+++ src/stratum.h
@@ -1,23 +1,12 @@
 #ifndef STRATUM_H
 #define STRATUM_H
-
 #include "bitcoin.h"
 #include <netinet/in.h>
-
-// 定义最大客户端连接数 (Solo 模式下 64 足够)
 #define MAX_CLIENTS 64
-
 typedef struct {
-    int sock;
-    struct sockaddr_in addr;
-    int id;
-    char extranonce1_hex[16]; // 分配给该矿机的唯一 ID
-    bool is_authorized;
-    bool active;
-    pthread_t thread_id;
+    int sock; struct sockaddr_in addr; int id;
+    char extranonce1_hex[16]; bool is_authorized; bool active; pthread_t thread_id;
 } Client;
-
 int stratum_start_thread();
 void stratum_broadcast_job(Template *tmpl);
-
 #endif

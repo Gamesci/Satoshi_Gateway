76a5b3760af8d744cdcb4d06cbc7910f09fce064
WuKong <govcnovo@gmail.com>
2025-12-13 04:50:10 +0800

    Update sha256.c

--- src/sha256.c
+++ src/sha256.c
@@ -1,6 +1,10 @@
 #include "sha256.h"
 #include <openssl/sha.h>
 void sha256_double(const void *data, size_t len, uint8_t *output) {
-    uint8_t hash1[SHA256_DIGEST_LENGTH]; SHA256(data, len, hash1); SHA256(hash1, SHA256_DIGEST_LENGTH, output);
+    uint8_t hash1[SHA256_DIGEST_LENGTH]; 
+    SHA256(data, len, hash1); 
+    SHA256(hash1, SHA256_DIGEST_LENGTH, output);
 }
-void sha256_single(const void *data, size_t len, uint8_t *output) { SHA256(data, len, output); }
+void sha256_single(const void *data, size_t len, uint8_t *output) { 
+    SHA256(data, len, output); 
+}

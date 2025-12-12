b6c5e72a2f82157cb4d61eaa5fb4bd168b0da684
WuKong <govcnovo@gmail.com>
2025-12-13 04:50:31 +0800

    Update bitcoin.h

--- src/bitcoin.h
+++ src/bitcoin.h
@@ -1,36 +1,38 @@
 #ifndef BITCOIN_H
 #define BITCOIN_H
+
 #include <stdbool.h>
 #include <stdint.h>
-#include <pthread.h>
 
-#define MAX_JOB_HISTORY 3
+#define MAX_JOB_HISTORY 16
 
 typedef struct {
     char job_id[32];
-    bool valid;
-    uint8_t prev_hash_bin[32]; // Internal LE
-    uint32_t version_val;
-    uint32_t nbits_val;
-    uint32_t curtime_val;
-    uint32_t height;
-    
-    char prev_hash_stratum[65]; // Stratum Swap32 Hex
-    char version_hex[9];        // Stratum Hex
-    char nbits_hex[9];
-    char ntime_hex[9];
-    
-    char coinb1[4096];
-    char coinb2[4096];
-    char merkle_branch[20][65]; 
+    char prev_hash_stratum[65]; // for stratum (swapped)
+    uint8_t prev_hash_bin[32];  // for block (LE)
+    char coinb1[8192];
+    char coinb2[8192];
+    char merkle_branch[16][65];
     int merkle_count;
-    char **tx_hexs;
-    int tx_count;
+    char version_hex[9];
+    uint32_t version_val;
+    char nbits_hex[9];
+    uint32_t nbits_val;
+    char ntime_hex[9];
+    uint32_t curtime_val;
     bool clean_jobs;
+    bool valid;
+    
+    // Internal
+    uint32_t height;
+    int64_t coinbase_value;
+    char **tx_hexs;
+    size_t tx_count;
 } Template;
 
 int bitcoin_init();
 void bitcoin_update_template(bool force_clean);
-int bitcoin_validate_and_submit(const char *job_id, const char *full_extranonce, const char *ntime, uint32_t nonce, uint32_t version_mask);
 bool bitcoin_get_latest_job(Template *out);
+int bitcoin_validate_and_submit(const char *job_id, const char *full_extranonce, const char *ntime, uint32_t nonce, uint32_t version_bits);
+
 #endif

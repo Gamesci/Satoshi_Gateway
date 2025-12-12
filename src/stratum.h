3b3b1dbf303315180ceac14378eb765b6ee2794a
WuKong <govcnovo@gmail.com>
2025-12-13 04:51:29 +0800

    Update stratum.h

--- src/stratum.h
+++ src/stratum.h
@@ -1,12 +1,30 @@
 #ifndef STRATUM_H
 #define STRATUM_H
-#include "bitcoin.h"
+
 #include <netinet/in.h>
-#define MAX_CLIENTS 64
+#include <stdbool.h>
+#include <pthread.h>
+#include <time.h>
+#include "bitcoin.h"
+
+#define MAX_CLIENTS 1024
+
 typedef struct {
-    int sock; struct sockaddr_in addr; int id;
-    char extranonce1_hex[16]; bool is_authorized; bool active; pthread_t thread_id;
+    int id;
+    int sock;
+    struct sockaddr_in addr;
+    bool active;
+    bool is_authorized;
+    pthread_t thread_id;
+    char extranonce1_hex[10];
+    
+    // VarDiff Stats
+    double current_diff;
+    time_t last_retarget_time;
+    int shares_in_window;
 } Client;
+
+void stratum_send_mining_notify(int sock, Template *tmpl);
 int stratum_start_thread();
-void stratum_broadcast_job(Template *tmpl);
+
 #endif

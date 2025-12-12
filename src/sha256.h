f540132066c58d13c759227aa78db728efa9bff5
WuKong <govcnovo@gmail.com>
2025-12-13 04:50:18 +0800

    Update sha256.h

--- src/sha256.h
+++ src/sha256.h
@@ -1,7 +1,7 @@
-#ifndef SHA256_WRAPPER_H
-#define SHA256_WRAPPER_H
-#include <stddef.h>
+#ifndef SHA256_H
+#define SHA256_H
 #include <stdint.h>
+#include <stddef.h>
 void sha256_double(const void *data, size_t len, uint8_t *output);
 void sha256_single(const void *data, size_t len, uint8_t *output);
 #endif

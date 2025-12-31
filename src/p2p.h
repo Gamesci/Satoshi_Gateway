850cb3d1cf15b092e93d7a693099bc6fd70f278b
WuKong <govcnovo@gmail.com>
2025-12-31 12:26:25 +0800

    Update p2p.h

--- src/p2p.h
+++ src/p2p.h
@@ -6,10 +6,11 @@
 
 /**
  * 启动 P2P 监听线程
- * @param ip 比特币节点 IP (如 "127.0.0.1")
- * @param port P2P 端口 (主网 8333, Testnet 18333)
- * @param magic 网络魔数 (主网 0xD9B4BEF9)
+ * @param host 比特币节点 IP 或域名
+ * @param port P2P 端口
+ * @param magic 网络魔数
+ * @param start_height 当前区块高度 (用于告知节点我们也已同步，请求实时推送)
  */
-int p2p_start_thread(const char *ip, int port, uint32_t magic);
+int p2p_start_thread(const char *host, int port, uint32_t magic, int32_t start_height);
 
 #endif
